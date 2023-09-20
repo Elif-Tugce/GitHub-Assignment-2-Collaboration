3890381c17f00d00f693a62516b6b8afa956a547
Mehmet Batuhan Neziroğlu <batuhan.neziroglu@ug.bilkent.edu.tr>
2023-09-20 15:52:24 +0300

    İskelet Oluşturuldu
    Metotlar için madde a/b/c/d/e diye iskelet oluşturuldu

--- MethodsClass.java
+++ MethodsClass.java
@@ -1,1 +1,53 @@
+/**
+ * MethodsClass
+ */
+public class MethodsClass {
 
+    //Madde C
+    public int minArray(int arr[]){
+
+
+
+
+        return 1;
+    }
+
+
+    public int maxArray(int arr[]){
+
+
+
+
+        return 1;
+    }
+
+
+    //Madde D
+
+    public int averageOfArray(int arr[]){
+
+
+        return 1;
+    }
+
+    public int[] differenceOfArray(int arr[], int average){
+
+
+        return /*array döncek*/;  
+    }
+
+    //Madde E
+    public int oddsIndexsSum(int arr[]){
+
+
+        return 1;
+    }
+
+    public int evensIndexsSum(int arr[]){
+
+
+        return 1;
+    }
+
+
+}
