2af79f301d45200d24225925693d230df4dd6d04
Emir Atlas <emiratlas11@gmail.com>
2023-09-20 15:58:22 +0300

    Option A
    Create Array Method

--- MethodsClass.java
+++ MethodsClass.java
@@ -1,1 +1,13 @@
+import java.util.Random;
 
+public class MethodsClass {
+    public static int[] createArray(int size) {
+        int[] arr = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+
+}
