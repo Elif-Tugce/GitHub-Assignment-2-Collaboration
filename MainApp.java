fc14598b01f4d7b4c5469fe0ff4ae1f8a689571e
Ece <ecebulut1@gmail.com>
2023-09-20 17:03:55 +0300

    Option 2 and Do-While

--- MainApp.java
+++ MainApp.java
@@ -8,34 +8,39 @@ public class MainApp{
         int random;
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
+        int average;
 
         System.out.println("Enter:");
         System.out.println("1 to find the max and min elements of the given array");
         System.out.println("2 to find the average of the elements in the given array");
         System.out.println("3 to find the sum of elements with odd- and even-numbered indexes in the given array");
         System.out.println("4 to EXIT");
-        userChoice = input.nextInt();
+        
 
         int [] arr = new int [arrSize];
 
         arr = MethodsClass.createArray(arrSize);
 
+ 
+        do{
 
-        if (userChoice == 1){
-            System.out.printf("The min element of the given array is: %d\n", MethodsClass.minArray(arr) );
-            System.out.printf("The max element of the given array is: %d\n", MethodsClass.maxArray(arr) );
+            userChoice = input.nextInt();
+            if (userChoice == 1){
+                System.out.printf("The min element of the given array is: %d\n", MethodsClass.minArray(arr) );
+                System.out.printf("The max element of the given array is: %d\n", MethodsClass.maxArray(arr) );
 
-        }
-        else if(userChoice == 2){
-            
-        }
-        else if(userChoice == 3){
+            }
+            else if(userChoice == 2){
+                average = MethodsClass.averageOfArray(arr);
+                System.out.printf("The average of the elements in the given array is: %d\n",average);
+            }
+            else if(userChoice == 3){
 
-        }
-        else if(userChoice == 4){
-            System.out.println("EXIT. Have a nice day!");
-        }
-
+            }
+            else if(userChoice == 4){
+                System.out.println("EXIT. Have a nice day!");
+            }
+        } while(userChoice != 4);
 
     }
 }
